9a9aeba5a267f0d8d349390837171a66315c5be9
DANIEL-FG\o9708 <o97082@yahoo.com.tw>
2018-03-28 16:13:40 +0800

    Added focus camera

---
+++ Assets/Camera/CameraPan.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPan : MonoBehaviour
+{
+    private GameObject player;
+    
+	void Start ()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        
+	}
+    private void LateUpdate()
+    {
+        transform.LookAt(player.transform);
+    }
+}
